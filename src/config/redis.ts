37d70030f559e0b5e1425c9630473af166aef686
asifdevx <xashadulbusiness@gmail.com>
2026-07-29 04:08:01 +0600

    fix redis again

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,13 +1,13 @@
 import IORedis from "ioredis";
-const redisConfig = {
-  host: process.env.REDIS_HOST||"127.0.0.1",
+
+const redis = new IORedis({
+    host: process.env.REDIS_HOST||"127.0.0.1",
   port: Number(process.env.REDIS_PORT) || 6379,
   username: "default",
   password: "gQAAAAAAAmNKAAIgcDJhZGNiNTZiODQzMzU0ODFmOTM4MjdiYzgzZDljM2MyNA",
   tls: {}, // Required for Upstash
   maxRetriesPerRequest: null,
-}
-const redis = new IORedis(redisConfig);
+});
 
 redis.on("connect", () => {
   console.log("[✅ Redis] connected");
@@ -17,10 +17,9 @@ redis.on("error", (err) => {
   console.error("❌ Redis error:", err);
 });
 
-export const redisConnection = {host: process.env.REDIS_HOST||"127.0.0.1",
+export const redisConnection = {
+  host: process.env.REDIS_HOST || "127.0.0.1",
   port: Number(process.env.REDIS_PORT) || 6379,
-  username: "default",
-  password: "gQAAAAAAAmNKAAIgcDJhZGNiNTZiODQzMzU0ODFmOTM4MjdiYzgzZDljM2MyNA",
-  tls: {},};
+};
 
 export { redis };
