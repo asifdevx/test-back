82ec2524e64c74f011a0fe9cc93751db576204cf
asifdevx <xashadulbusiness@gmail.com>
2026-07-30 11:30:05 +0600

    last

--- src/router/rou.admin.ts
+++ src/router/rou.admin.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import { getDozSwapRestrictionStatus, listPositions, logMint, logRemove, updateDozSwapRestrictionStatus } from "../mongoDb/controllers/c.admin-poolAdmin";
-
+import { Request,Response } from "express";
 import { exportSwapTransactionsCsv, listSwapTransactions } from "../mongoDb/controllers/c.admin-swapTx";
 import { getAllChains, getTokenCardDetails, getTokenDetail } from "../mongoDb/controllers/c.swap";
+import { Chain } from "../mongoDb/schemas/sch.paymentChain";
 
 
 const router = Router();
@@ -23,7 +24,32 @@ router.get("/admin/positions", listPositions);
 
 
 
-router.get("/swap", getAllChains);
+router.get("/swap", async(req: Request, res: Response) =>{
+  try {
+    const chains = await Chain.find({ isActive: true }).lean();
+
+    if (!chains.length) {
+      return res.status(404).json({
+        success: false,
+        message: "No chains found",
+        data: [],
+      });
+    }
+
+    return res.json({
+      success: true,
+      message: "Chains fetched successfully",
+      data: chains,
+    });
+  } catch (error) {
+    console.error("GET /chains error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+});
 // router.get("/tokens", getTokenCardDetails);
 // router.get("/tokens/:chainId/:contractAddress", getTokenDetail);
 export default router;
